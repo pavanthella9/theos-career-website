a9a5654a1c907cac227f7d9e7e4e11abb03e9c4a
pavanthella9 <pavanthella.9@gmail.com>
2026-08-03 18:39:42 -0400

    Correct official contact email

--- lib/site.ts
+++ lib/site.ts
@@ -3,7 +3,7 @@ export const site = {
   tagline: "Your Global Career Gateway",
   phoneDisplay: "+91 90007 05666",
   phoneLink: "+919000705666",
-  email: "contact@theoscareerconsultats.com",
+  email: "contact@theoscareerconsultants.com",
   address: "2nd Floor, Sreerama Towers, RC Road, Tirupati 517501",
   markets: ["India", "Singapore", "Malaysia"],
   whatsappMessage: "Hello Theos Career Consultants, I would like to know more about your training and placement assistance services.",
